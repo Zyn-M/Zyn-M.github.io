0eadb5ed9fedcea28d3900864ebd0d037b4da990
Zyn-M <martinezynnia154@gmail.com>
2022-03-10 01:21:20 +0000

    fix bug

--- projects/platformer/js/factory/platform.js
+++ projects/platformer/js/factory/platform.js
@@ -8,11 +8,10 @@
         game.platforms = game.add.group();
         game.platforms.enableBody = true;
         
-        function create(x, y, scaleX, scaleY, immovable, backgroundColor) {
+        function create(x, y, scaleX, scaleY, immovable) {
             var platform = game.platforms.create(x, y, 'platform');
             platform.scale.setTo(scaleX || 1, scaleY || 1);
             platform.body.immovable = immovable || true;
-            platform.backgroundColor = "#fae1ef";
             return platform;
         }
         opspark.platform.create = create;
